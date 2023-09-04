2383abd43b8649c4601b875764b712b3affb8219
Abhinav Singh <kittusinghranchi@gmail.com>
2023-09-04 15:39:23 +0530

    fourth section css done

--- dashboard/src/Components/FourthSection/Components/Popunder.js
+++ dashboard/src/Components/FourthSection/Components/Popunder.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import "./CSS/Popunder.css";
 const Popunder = () => {
   return (
-    <div className="flex flex-row items-center justify-center gap-20">
+    <>
+    <div className="dekstop-view flex flex-row items-center justify-center gap-20">
       <div className="flex flex-col justify-center items-center gap-5">
         <div className="flex">
           <img src="/assets/images/popunderImg.png" className="w-[18vw]" />
@@ -28,6 +29,36 @@ const Popunder = () => {
         </div>
       </div>
     </div>
+
+
+
+    <div className="mobile-view flex flex-col items-center justify-center gap-20">
+      <div className="flex flex-col justify-center items-center gap-5">
+        <div className="flex">
+          <img src="/assets/images/popunderImg.png" />
+        </div>
+        <div className="font-sm font-semibold text-[#0000007d] text-center">
+          When User clicks on the website, Ads open in new tab.
+        </div>
+      </div>
+
+      <div className="flex flex-col h-[100%] justify-center items-center gap-10">
+        <div className="flex justify-center items-center text-center text-2xl font-bold text-[#9e4bf1] w-[100%]">
+          Popunder
+        </div>
+        <div className="flex  justify-center items-center text-center text-[#5266a9] w-[100%]">
+          Popunder ads appear in a fresh browser window or tab, often concealed
+          behind the main window. The Affworld popunder script ensures complete
+          responsiveness at 100%.
+        </div>
+
+        <div className="flex flex-row w-[100%]  justify-center items-center text-center gap-20">
+            <button className="text-semibold text-[#413737]">Demo</button>
+            <button className="text-semibold text-[#9e4bf1]">More Info</button>
+        </div>
+      </div>
+    </div>
+    </>
   );
 };
 
--- dashboard/src/Components/FourthSection/FourthSection.js
+++ dashboard/src/Components/FourthSection/FourthSection.js
@@ -1,38 +1,249 @@
-import React, { useState } from 'react'
-import Popunder from './Components/Popunder';
-import SocialBar from './Components/SocialBar';
-import InPagePush from './Components/InPagePush';
-import NativeBanners from './Components/NativeBanners';
-import Banners from './Components/Banners';
-
+import React, { useState } from "react";
+import Popunder from "./Components/Popunder";
+import SocialBar from "./Components/SocialBar";
+import InPagePush from "./Components/InPagePush";
+import NativeBanners from "./Components/NativeBanners";
+import Banners from "./Components/Banners";
+import "../../CSS/FourthSection.css";
 const FourthSection = () => {
-    const [active,setActive]=useState(1);
-    function handleClick(number){
-        setActive(number);
-    }
+  const [active, setActive] = useState(1);
+  function handleClick(number) {
+    setActive(number);
+  }
   return (
-    <div className='flex flex-col justify-between items-center m-[5%] p-[2%]'>
-      <div className='flex w-[100%] text-[2vw] text-left font-semibold'>
-        <span className='text-[#9e4bf1]'>Online Ads </span>&nbsp;<span className='text-[#392976]'> for publishers</span>
-      </div>
-      <div className='flex flex-row justify-center text-[#5266a9] item gap-10 mt-[6%] font-semibold text-xl'>
-        <div className={`flex justify-center text-[#5266a9] items-center p-4 m-2 ${active===1?"border-4 rounded-full border-[#9e4bf1]" :"border-none"}`}>
-            <button onClick={()=>{handleClick(1)}}>Popunder</button></div>
-        <div className={`flex justify-center text-[#5266a9] items-center p-4 ${active===2?"border-4 rounded-full border-[#9e4bf1]" :"border-none"}`}><button onClick={()=>{handleClick(2)}}>Social Bar</button></div>
-        <div className={`flex justify-center text-[#5266a9] items-center p-4 ${active===3?"border-4 rounded-full border-[#9e4bf1]" :"border-none"}`}><button onClick={()=>{handleClick(3)}}>In-Page Push(Social Bar)</button></div>
-        <div className={`flex justify-center text-[#5266a9] items-center p-4 ${active===4?"border-4 rounded-full border-[#9e4bf1]" :"border-none"}`}><button onClick={()=>{handleClick(4)}}>Native Banners</button></div>
-        <div className={`flex justify-center text-[#5266a9] items-center p-4 ${active===5?"border-4 rounded-full border-[#9e4bf1]" :"border-none"}`}><button onClick={()=>{handleClick(5)}}>Banners</button></div>
+    <>
+      <div className="dekstop-view flex flex-col justify-between items-center m-[5%] p-[2%]">
+        <div className="flex w-[100%] text-[2vw] text-left font-semibold">
+          <span className="text-[#9e4bf1]">Online Ads </span>&nbsp;
+          <span className="text-[#392976]"> for publishers</span>
+        </div>
+        <div className="flex flex-row justify-center text-[#5266a9] item gap-10 mt-[6%] font-semibold text-xl">
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-4 m-2 ${
+              active === 1
+                ? "border-4 rounded-full border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(1);
+              }}
+            >
+              Popunder
+            </button>
+          </div>
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-4 ${
+              active === 2
+                ? "border-4 rounded-full border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(2);
+              }}
+            >
+              Social Bar
+            </button>
+          </div>
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-4 ${
+              active === 3
+                ? "border-4 rounded-full border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(3);
+              }}
+            >
+              In-Page Push(Social Bar)
+            </button>
+          </div>
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-4 ${
+              active === 4
+                ? "border-4 rounded-full border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(4);
+              }}
+            >
+              Native Banners
+            </button>
+          </div>
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-4 ${
+              active === 5
+                ? "border-4 rounded-full border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(5);
+              }}
+            >
+              Banners
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center mt-[4%]">
+          {active === 1 && (
+            <>
+              <Popunder />
+            </>
+          )}
+          {active === 2 && (
+            <>
+              <SocialBar />
+            </>
+          )}
+          {active === 3 && (
+            <>
+              <InPagePush />
+            </>
+          )}
+          {active === 4 && (
+            <>
+              <NativeBanners />
+            </>
+          )}
+          {active === 5 && (
+            <>
+              <Banners />
+            </>
+          )}
+        </div>
       </div>
 
-      <div className='flex justify-center items-center mt-[4%]'>
-            {active===1 && <><Popunder/></>}
-            {active===2 && <><SocialBar/></>}
-            {active===3 && <><InPagePush/></>}
-            {active===4 && <><NativeBanners/></>}
-            {active===5 && <><Banners/></>}
-      </div>
-    </div>
-  )
-}
+      <div className="mobile-view flex flex-col justify-between items-center m-[5%] p-[2%]">
+        <div className="flex w-[100%] text-2xl text-left font-semibold">
+          <span className="text-[#9e4bf1]">Online Ads </span>&nbsp;
+          <span className="text-[#392976]"> for publishers</span>
+        </div>
+        <div className="flex flex-row justify-center text-[#5266a9] item gap-10 mt-[6%] font-semibold text-sm">
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-1 m-2 ${
+              active === 1
+                ? "border-4 rounded-md border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(1);
+              }}
+            >
+              Popunder
+            </button>
+          </div>
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-1 ${
+              active === 2
+                ? "border-4 rounded-md border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(2);
+              }}
+            >
+              Social Bar
+            </button>
+          </div>
 
-export default FourthSection
+          
+        </div>
+
+        <div className="flex flex-row justify-center text-[#5266a9] item gap-2 mt-[6%] font-semibold text-sm">
+          
+
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-1 ${
+              active === 3
+                ? "border-4 rounded-md border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(3);
+              }}
+            >
+              In-Page Push(Social Bar)
+            </button>
+          </div>
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-1 ${
+              active === 4
+                ? "border-4 rounded-md border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(4);
+              }}
+            >
+              Native Banners
+            </button>
+          </div>
+          <div
+            className={`flex justify-center text-[#5266a9] items-center p-1 ${
+              active === 5
+                ? "border-4 rounded-md border-[#9e4bf1]"
+                : "border-none"
+            }`}
+          >
+            <button
+              onClick={() => {
+                handleClick(5);
+              }}
+            >
+              Banners
+            </button>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center mt-[4%]">
+          {active === 1 && (
+            <>
+              <Popunder />
+            </>
+          )}
+          {active === 2 && (
+            <>
+              <SocialBar />
+            </>
+          )}
+          {active === 3 && (
+            <>
+              <InPagePush />
+            </>
+          )}
+          {active === 4 && (
+            <>
+              <NativeBanners />
+            </>
+          )}
+          {active === 5 && (
+            <>
+              <Banners />
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FourthSection;
